fb5e3681a5db77b814ece32b417370970f6c1b56
HarshaRatnal <135690201+HarshaRatnal@users.noreply.github.com>
2023-06-28 02:05:37 -0400

    Add files via upload

---
+++ client/public/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Register from './components/Register.js';
+import Navbar from './components/Navbar.js';
+import Login from './components/Login.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <Register />
+     <Login />
+     
+     
+      
+    
+
+    </div>
+  
+  );
+}
+
+export default App;
