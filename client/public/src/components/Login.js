4c82035d8b44d50e5464a3711bbc3c86febd54dd
HarshaRatnal <135690201+HarshaRatnal@users.noreply.github.com>
2023-06-28 02:05:04 -0400

    Add files via upload

--- client/public/src/components/Login.js
+++ client/public/src/components/Login.js
@@ -1,1 +1,35 @@
+const Login = () =>{
+    return(
+        <div>
+         <form className = "login-form">
 
+        <div className="mb-3">
+          <label For="username" className="form-label">Username</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            id="username"
+            name='username'
+            required
+          />
+          </div>
+          
+       
+        <div className="mb-3">
+          <label For="password" className="form-label">Password</label>
+          <input 
+            type="password" 
+            className="form-control" 
+            id="password"
+            name='password'
+            required
+          />
+        </div>
+
+       
+        <input type="submit" className="btn btn-primary" value="login"/>
+        </form>
+        </div>
+    );
+}
+export default Login;
